468ef6e5f71bef999a03164658c5a74b0a1003e4
bilgeiremgoc <99967616+bilgeiremgoc@users.noreply.github.com>
2025-06-22 16:27:57 +0300

    Update deg_GSE7305.R

--- deg_GSE7305/deg_GSE7305.R
+++ deg_GSE7305/deg_GSE7305.R
@@ -1,8 +1,6 @@
 #Dataseti elde etme ve matrix oluşturma
 gse <- getGEO("GSE7305")
-
 gse1em <- getGEO("GSE7305", GSEMatrix = TRUE, AnnotGPL = TRUE)
-
 gse_data <- gse1em[[1]]
 
 exprs_data <- exprs(gse_data)
@@ -10,7 +8,6 @@ boxplot(exprs_data, outline = FALSE, las=2, main="Before Normalization")
 exprs_data <- log2(exprs_data + 1)
 exprs_data <- normalizeBetweenArrays(exprs_data)
 boxplot(exprs_data, outline = FALSE, las=2, main="After Normalization")
-
 
 metadata <- pData(gse_data)
 group <- ifelse(grepl("normal", metadata$characteristics_ch1, ignore.case = TRUE), "normal", "endometriosis")
@@ -32,50 +29,37 @@ head(results)
 deg <- subset(results, abs(logFC) > 1 & adj.P.Val < 0.05)
 deg <- deg %>% filter(!is.na(deg$GeneSymbol)) 
 head(deg)
-
 write_xlsx(deg, "deg_GSE7305.xlsx")
 
 deg_filtered <- deg %>% filter(!is.na(deg$GeneSymbol))
-
 deg_filtered <- deg %>%
   filter(adj.P.Val < 0.05 & abs(logFC) > 1)
 nrow(deg_filtered)
 
 up_genes <- deg_filtered %>% filter(logFC> 1)
 down_genes <- deg_filtered %>% filter(logFC < -1)
-
 write_xlsx(up_genes, "deg_GSE7305_up_genes.xlsx")
 write_xlsx(down_genes, "deg_GSE7305_down_genes.xlsx")
 
 # Volkan Plotu
 results$threshold <- as.factor(abs(results$logFC) > 1 & results$adj.P.Val < 0.05)
-
 ggplot(results, aes(x = logFC, y = -log10(adj.P.Val), color = threshold)) +
   geom_point(alpha = 0.8) +
   theme_minimal() +
   scale_color_manual(values = c("grey", "red")) +
   labs(title = "Volcano Plot")
 
-
 #Heatmap (ilk 50 DEG)
 top50 <- deg[order(deg$adj.P.Val), ][1:50, ]
 deg_ids <- rownames(top50)
-
 heatmap_data <- exprs_data[deg_ids, ]
-
 heatmap_data <- t(scale(t(heatmap_data)))
-
 pheatmap(heatmap_data, 
          scale = "none", 
          show_rownames = TRUE, 
          cluster_rows = TRUE, 
          cluster_cols = TRUE, 
          main = "Top 50 DEG Heatmap")
-
-
-
-
-
 
 deg$ENTREZID <- mapIds(
   hgu133plus2.db,
@@ -86,7 +70,6 @@ deg$ENTREZID <- mapIds(
 )
 entrez_ids <- deg$ENTREZID
 
-
 #GO_BP
 go_result_BP <- enrichGO(gene = entrez_ids,
                          OrgDb = org.Hs.eg.db,
@@ -95,11 +78,9 @@ go_result_BP <- enrichGO(gene = entrez_ids,
                          pAdjustMethod = "BH",
                          qvalueCutoff = 0.05,
                          readable = TRUE)
-
 head(go_result_BP)
 barplot(go_result_BP, showCategory = 20, title = "GO BP Enrichment")
 write_xlsx(as.data.frame(go_result_BP), "deg_GSE7305_GO_BP_enrichment.xlsx")
-
 #GO_MF
 go_result_MF<- enrichGO(gene = entrez_ids,
                         OrgDb = org.Hs.eg.db,
@@ -112,7 +93,6 @@ go_result_MF<- enrichGO(gene = entrez_ids,
 head(go_result_MF)
 barplot(go_result_MF, showCategory = 20, title = "GO MF Enrichment")
 write_xlsx(as.data.frame(go_result_MF), "deg_GSE7305_GO_MF_enrichment.xlsx")
-
 #GO_CC
 go_result_CC <- enrichGO(gene = entrez_ids,
                          OrgDb = org.Hs.eg.db,
@@ -125,7 +105,6 @@ go_result_CC <- enrichGO(gene = entrez_ids,
 head(go_result_CC)
 barplot(go_result_CC, showCategory = 20, title = "GO CC Enrichment")
 write_xlsx(as.data.frame(go_result_CC), "deg_GSE7305_GO_CC_enrichment.xlsx")
-
 #KEGG enrichment
 kegg_enrich <- enrichKEGG(gene = entrez_ids,
                           organism = "hsa",
@@ -135,7 +114,6 @@ kegg_enrich <- enrichKEGG(gene = entrez_ids,
 kegg_enrich <- setReadable(kegg_enrich, OrgDb = org.Hs.eg.db, keyType = "ENTREZID")
 head(kegg_enrich)
 barplot(kegg_enrich, showCategory = 20, title = "KEGG Pathway Enrichment")
-
 write_xlsx(as.data.frame(kegg_enrich), "deg_GSE73058_KEGG_enrichment.xlsx")
 
 #Reactome Enrichment
@@ -146,27 +124,14 @@ reactome_enrich <- enrichPathway(gene = entrez_ids,
 barplot(reactome_enrich, showCategory = 20, title = "Reactome Pathway Enrichment")
 write_xlsx(as.data.frame(reactome_enrich), "deg_GSE7305_reactome_enrichment.xlsx")
 
-
-
-
-
-
-
-
-
-
-
-
 #xCell İnfiltrasyonu
 gene_symbols <- getSYMBOL(rownames(exprs_data), "hgu133plus2.db")
 exprs_data_symbols <- exprs_data
 rownames(exprs_data_symbols) <- gene_symbols
-
 exprs_data_symbols <- exprs_data_symbols[!is.na(rownames(exprs_data_symbols)), ]
 exprs_data_symbols <- rowsum(exprs_data_symbols, group = rownames(exprs_data_symbols))
 
 xcell_result <- xCellAnalysis(exprs_data_symbols)
-
 xcell_df <- as.data.frame(xcell_result)
 xcell_df <- cbind(Cell_Type = rownames(xcell_result), xcell_df)
 
@@ -175,14 +140,9 @@ pheatmap(xcell_result,
          scale = "row",
          clustering_distance_rows = "euclidean",
          clustering_distance_cols = "euclidean")
-
 write_xlsx(xcell_df, "deg_GSE7305_xCell_Results.xlsx")
 
-
-
-
 xcell_result <- xCellAnalysis(exprs_data_symbols)
-
 xcell_matrix <- as.data.frame(t(xcell_result))
 xcell_matrix$sample <- rownames(xcell_matrix)
 xcell_matrix$group <- group 
@@ -190,8 +150,6 @@ head(xcell_matrix)
 
 long_xcell <- xcell_matrix %>%
   pivot_longer(cols = -c(sample, group), names_to = "cell_type", values_to = "proportion")
-
-
 stat_tests <- long_xcell %>%
   group_by(cell_type) %>%
   summarise(
@@ -201,9 +159,7 @@ stat_tests <- long_xcell %>%
   ) %>%
   mutate(adj_p = p.adjust(p_value, method = "fdr")) %>%
   arrange(p_value)
-
 write_xlsx(stat_tests, "deg_GSE7305_xcell_stat_tests.xlsx")
-
 
 ggplot(long_xcell, aes(x = group, y = proportion, fill = group)) +
   geom_boxplot(outlier.shape = NA) +
@@ -211,6 +167,5 @@ ggplot(long_xcell, aes(x = group, y = proportion, fill = group)) +
   theme_minimal() +
   labs(title = "Immune Cell Infiltration (xCell) - GSE7305") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
 
 
